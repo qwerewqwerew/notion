2a12503c5c1e11d37a42bd27bc8957a4502c50a9
student-studies-everyday <student.studies.everyday@gmail.com>
2024-08-04 23:21:32 +0900

    ss

--- components/Block.js
+++ components/Block.js
@@ -1,8 +1,8 @@
 // components/Block.js
 
 import { useState } from 'react';
-import Link from 'next/link';
-import pageStyles from '../styles/Page.module.css'; // Page styles
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import blockStyles from '../styles/Block.module.css'; // Block styles
 
 const Block = ({ block }) => {
@@ -11,17 +11,18 @@ const Block = ({ block }) => {
 	switch (type) {
 		case 'paragraph':
 			return (
-				<div key={id} className={blockStyles.paragraph}>
+				<p key={id} className={blockStyles.paragraph}>
 					{block.paragraph.rich_text.map((text) => text.plain_text).join('')}
-				</div>
+				</p>
 			);
 
 		case 'heading_1':
 		case 'heading_2':
 		case 'heading_3':
-			const HeadingTag = type.replace('_', '');
+			const HeadingTag = `h${type.split('_')[1]}`;
+			const headingClass = blockStyles[`heading${type.split('_')[1]}`];
 			return (
-				<HeadingTag key={id} className={blockStyles.heading}>
+				<HeadingTag key={id} className={headingClass}>
 					{block[type].rich_text.map((text) => text.plain_text).join('')}
 				</HeadingTag>
 			);
@@ -58,24 +59,64 @@ const Block = ({ block }) => {
 				</div>
 			);
 
+		case 'image':
+			const imageUrl = block.image.file?.url || block.image.external?.url;
+			return <img key={id} src={imageUrl} alt='Notion image' className={blockStyles.image} />;
+
+		case 'code':
+			const language = block.code.language || 'javascript'; // 기본 언어 설정
+			const codeText = block.code.rich_text.map((text) => text.plain_text).join('');
+			return (
+				<SyntaxHighlighter key={id} language={language} style={oneDark} className={blockStyles.code}>
+					{codeText}
+				</SyntaxHighlighter>
+			);
+
+		case 'divider':
+			return <hr key={id} className={blockStyles.divider} />;
+
+		case 'callout':
+			return (
+				<div key={id} className={blockStyles.callout}>
+					{block.callout.rich_text.map((text) => text.plain_text).join('')}
+				</div>
+			);
+
+		case 'embed':
+			return <iframe key={id} src={block.embed.url} className={blockStyles.embed} frameBorder='0' allowFullScreen></iframe>;
+
+		case 'video':
+			const videoUrl = block.video.file?.url || block.video.external?.url;
+			return (
+				<video key={id} controls className={blockStyles.video}>
+					<source src={videoUrl} type='video/mp4' />
+				</video>
+			);
+
+		case 'file':
+			const fileUrl = block.file.file?.url || block.file.external?.url;
+			return (
+				<a key={id} href={fileUrl} className={blockStyles.file} target='_blank' rel='noopener noreferrer'>
+					Download File
+				</a>
+			);
+
+		case 'bookmark':
+			return (
+				<a key={id} href={block.bookmark.url} className={blockStyles.bookmark} target='_blank' rel='noopener noreferrer'>
+					{block.bookmark.url}
+				</a>
+			);
+
 		case 'child_page':
 			const childPageTitle = block.child_page?.title || 'Untitled';
 			return (
 				<div key={id} className={blockStyles.childPage}>
-					<h2>
-						<Link href={`/${block.id}`}>{childPageTitle}</Link>
-					</h2>
-					{block.child_page.blocks?.length > 0 && (
-						<div className={blockStyles.childBlocks}>
-							{block.child_page.blocks.map((childBlock) => (
-								<Block key={childBlock.id} block={childBlock} />
-							))}
-						</div>
-					)}
+					<a href={`/${block.id}`} className={blockStyles.childPageLink}  rel='noopener noreferrer'>
+						{childPageTitle}
+					</a>
 				</div>
 			);
-
-		// 추가 블록 타입 처리
 
 		default:
 			return <div key={id}>Unsupported block type: {type}</div>;
