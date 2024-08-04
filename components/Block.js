8c19a47721f70846af4523973e4bc12c96a3d746
student-studies-everyday <student.studies.everyday@gmail.com>
2024-08-04 18:00:40 +0900

    page

--- components/Block.js
+++ components/Block.js
@@ -1,131 +1,72 @@
 // components/Block.js
 
-import React from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
-import styles from '../styles/Page.module.css';
+import pageStyles from '../styles/Page.module.css'; // Page styles
+import blockStyles from '../styles/Block.module.css'; // Block styles
 
 const Block = ({ block }) => {
 	const { type, id } = block;
-	const value = block[type];
-
-	const renderRichText = (richTextArray) => {
-		return richTextArray.map((text) => text.plain_text).join('');
-	};
 
 	switch (type) {
 		case 'paragraph':
 			return (
-				<div key={id} className={styles.paragraph}>
-					{renderRichText(value.rich_text)}
+				<div key={id} className={blockStyles.paragraph}>
+					{block.paragraph.rich_text.map((text) => text.plain_text).join('')}
 				</div>
 			);
+
 		case 'heading_1':
-			return (
-				<div key={id} className={styles.heading1}>
-					{renderRichText(value.rich_text)}
-				</div>
-			);
 		case 'heading_2':
-			return (
-				<div key={id} className={styles.heading2}>
-					{renderRichText(value.rich_text)}
-				</div>
-			);
 		case 'heading_3':
+			const HeadingTag = type.replace('_', '');
 			return (
-				<div key={id} className={styles.heading3}>
-					{renderRichText(value.rich_text)}
-				</div>
+				<HeadingTag key={id} className={blockStyles.heading}>
+					{block[type].rich_text.map((text) => text.plain_text).join('')}
+				</HeadingTag>
 			);
-		case 'bulleted_list_item':
-			return (
-				<div key={id} className={styles.listItem}>
-					{renderRichText(value.rich_text)}
-				</div>
-			);
-		case 'numbered_list_item':
-			return (
-				<div key={id} className={styles.listItem}>
-					{renderRichText(value.rich_text)}
-				</div>
-			);
-		case 'to_do':
-			return (
-				<div key={id} className={styles.paragraph}>
-					<input type='checkbox' checked={value?.checked} readOnly className={styles.checkbox} />
-					<span>{renderRichText(value.rich_text)}</span>
-				</div>
-			);
-		case 'toggle':
-			return (
-				<div key={id} className={styles.toggle}>
-					<details>
-						<summary>{renderRichText(value.rich_text)}</summary>
-						{block.children?.map((child) => (
-							<Block key={child.id} block={child} />
-						))}
-					</details>
-				</div>
-			);
+
 		case 'quote':
 			return (
-				<div key={id} className={styles.blockquote}>
-					{renderRichText(value.rich_text)}
-				</div>
+				<blockquote key={id} className={blockStyles.quote}>
+					{block.quote.rich_text.map((text) => text.plain_text).join('')}
+				</blockquote>
 			);
-		case 'code':
+
+		case 'numbered_list_item':
+		case 'bulleted_list_item':
 			return (
-				<div key={id} className={styles.code}>
-					<pre>
-						<code>{renderRichText(value.rich_text)}</code>
-					</pre>
-				</div>
+				<li key={id} className={blockStyles.listItem}>
+					{block[type].rich_text.map((text) => text.plain_text).join('')}
+				</li>
 			);
-		case 'image':
-			const src = value?.type === 'external' ? value.external.url : value.file.url;
-			const caption = value?.caption?.length ? renderRichText(value.caption) : '';
+
+		case 'toggle':
+			const [isOpen, setIsOpen] = useState(false);
 			return (
-				<div key={id} className={styles.image}>
-					<figure>
-						<img src={src} alt={caption} loading='lazy' />
-						{caption && <figcaption>{caption}</figcaption>}
-					</figure>
+				<div key={id} className={blockStyles.toggle}>
+					<div className={blockStyles.toggleTitle} onClick={() => setIsOpen(!isOpen)}>
+						{block.toggle.rich_text.map((text) => text.plain_text).join('')}
+					</div>
+					{isOpen && (
+						<div className={blockStyles.toggleContent}>
+							{block.toggle.children.map((childBlock) => (
+								<Block key={childBlock.id} block={childBlock} />
+							))}
+						</div>
+					)}
 				</div>
 			);
-		case 'divider':
-			return (
-				<div key={id} className={styles.divider}>
-					<hr />
-				</div>
-			);
-		case 'file':
-			const fileSrc = value?.type === 'external' ? value.external.url : value.file.url;
-			return (
-				<div key={id} className={styles.bookmark}>
-					<a href={fileSrc}>{fileSrc}</a>
-				</div>
-			);
-		case 'bookmark':
-			return (
-				<div key={id} className={styles.bookmark}>
-					<a href={value?.url}>{value?.url}</a>
-				</div>
-			);
-		case 'embed':
-			return (
-				<div key={id} className={styles.embed}>
-					<iframe src={value?.url} title='Embed' width='100%' height='400px' />
-				</div>
-			);
+
 		case 'child_page':
 			const childPageTitle = block.child_page?.title || 'Untitled';
 			return (
-				<div key={id} className={styles.childPage}>
+				<div key={id} className={blockStyles.childPage}>
 					<h2>
 						<Link href={`/${block.id}`}>{childPageTitle}</Link>
 					</h2>
 					{block.child_page.blocks?.length > 0 && (
-						<div className={styles.childBlocks}>
+						<div className={blockStyles.childBlocks}>
 							{block.child_page.blocks.map((childBlock) => (
 								<Block key={childBlock.id} block={childBlock} />
 							))}
@@ -133,6 +74,9 @@ const Block = ({ block }) => {
 					)}
 				</div>
 			);
+
+		// 추가 블록 타입 처리
+
 		default:
 			return <div key={id}>Unsupported block type: {type}</div>;
 	}
--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,20 @@
-// pages/_app.js
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-	const router = useRouter();
-
 	useEffect(() => {
-		const handleRouteChange = (url) => {
-			window.gtag('config', 'G-1S60TZQ5P3', {
-				page_path: url,
-			});
-		};
-		router.events.on('routeChangeComplete', handleRouteChange);
-		return () => {
-			router.events.off('routeChangeComplete', handleRouteChange);
-		};
-	}, [router.events]);
+		// GA 초기화
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'G-1S60TZQ5P3');
+	}, []);
 
 	return (
 		<>
-			<Head>
-				{/* Google Analytics */}
-				<script async src='https://www.googletagmanager.com/gtag/js?id=G-1S60TZQ5P3'></script>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-1S60TZQ5P3');
-            `,
-					}}
-				/>
-			</Head>
+			<Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-1S60TZQ5P3' />
 			<Component {...pageProps} />
 		</>
 	);
