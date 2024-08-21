8c60ec4e72de025a4e2ac805cfcd7c1016d8fc18
qwerewqwerew <59301948+qwerewqwerew@users.noreply.github.com>
2024-08-21 18:02:55 +0900

    reset

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react'
 import dynamic from 'next/dynamic'
-import Head from 'next/head'
-// import Image from 'next/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import { ExtendedRecordMap } from 'notion-types'
-import { getPageTitle } from '../packages/notion-utils/src'
-import { NotionRenderer } from '~/packages/react-notion-x/src/renderer'
+import cs from 'classnames'
+import { PageBlock } from 'notion-types'
+import { formatDate, getBlockTitle, getPageProperty } from 'notion-utils'
+import BodyClassName from 'react-body-classname'
+import { NotionRenderer } from 'react-notion-x'
 import TweetEmbed from 'react-tweet-embed'
+import { useSearchParam } from 'react-use'
 
+import * as config from '@/lib/config'
+import * as types from '@/lib/types'
+import { mapImageUrl } from '@/lib/map-image-url'
+import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url'
+import { searchNotion } from '@/lib/search-notion'
+import { useDarkMode } from '@/lib/use-dark-mode'
+
+import Comments from './Comments'
+//import { Footer } from './Footer'
 import { Loading } from './Loading'
+import { NotionPageHeader } from './NotionPageHeader'
+import { Page404 } from './Page404'
+import { PageAside } from './PageAside'
+import { PageHead } from './PageHead'
+import styles from './styles.module.css'
 
 // -----------------------------------------------------------------------------
 // dynamic imports for optional components
@@ -18,8 +34,8 @@ import { Loading } from './Loading'
 
 const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then(async (m) => {
-    // additional prism syntaxes
-    await Promise.all([
+    // add / remove any prism syntaxes here
+    await Promise.allSettled([
       import('prismjs/components/prism-markup-templating.js'),
       import('prismjs/components/prism-markup.js'),
       import('prismjs/components/prism-bash.js'),
@@ -55,6 +71,7 @@ const Code = dynamic(() =>
     return m.Code
   })
 )
+
 const Collection = dynamic(() =>
   import('react-notion-x/build/third-party/collection').then(
     (m) => m.Collection
@@ -70,7 +87,11 @@ const Pdf = dynamic(
   }
 )
 const Modal = dynamic(
-  () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal),
+  () =>
+    import('react-notion-x/build/third-party/modal').then((m) => {
+      m.Modal.setAppElement('.notion-viewport')
+      return m.Modal
+    }),
   {
     ssr: false
   }
@@ -80,92 +101,183 @@ const Tweet = ({ id }: { id: string }) => {
   return <TweetEmbed tweetId={id} />
 }
 
-export const NotionPage = ({
+const propertyLastEditedTimeValue = (
+  { block, pageHeader },
+  defaultFn: () => React.ReactNode
+) => {
+  if (pageHeader && block?.last_edited_time) {
+    return `Last updated ${formatDate(block?.last_edited_time, {
+      month: 'long'
+    })}`
+  }
+
+  return defaultFn()
+}
+
+const propertyDateValue = (
+  { data, schema, pageHeader },
+  defaultFn: () => React.ReactNode
+) => {
+  if (pageHeader && schema?.name?.toLowerCase() === 'published') {
+    const publishDate = data?.[0]?.[1]?.[0]?.[1]?.start_date
+
+    if (publishDate) {
+      return `${formatDate(publishDate, {
+        month: 'long'
+      })}`
+    }
+  }
+
+  return defaultFn()
+}
+
+const propertyTextValue = (
+  { schema, pageHeader },
+  defaultFn: () => React.ReactNode
+) => {
+  if (pageHeader && schema?.name?.toLowerCase() === 'author') {
+    return <b>{defaultFn()}</b>
+  }
+
+  return defaultFn()
+}
+
+export const NotionPage: React.FC<types.PageProps> = ({
+  site,
   recordMap,
-  previewImagesEnabled,
-  rootPageId,
-  rootDomain
-}: {
-  recordMap: ExtendedRecordMap
-  previewImagesEnabled?: boolean
-  rootPageId?: string
-  rootDomain?: string
+  error,
+  pageId
 }) => {
   const router = useRouter()
+  const lite = useSearchParam('lite')
+
+  const components = React.useMemo(
+    () => ({
+      nextImage: Image,
+      nextLink: Link,
+      Code,
+      Collection,
+      Equation,
+      Pdf,
+      Modal,
+      Tweet,
+      Header: NotionPageHeader,
+      propertyLastEditedTimeValue,
+      propertyTextValue,
+      propertyDateValue
+    }),
+    []
+  )
+
+  // lite mode is for oembed
+  const isLiteMode = lite === 'true'
+
+  const { isDarkMode } = useDarkMode()
+
+  const siteMapPageUrl = React.useMemo(() => {
+    const params: any = {}
+    if (lite) params.lite = lite
+
+    const searchParams = new URLSearchParams(params)
+    return mapPageUrl(site, recordMap, searchParams)
+  }, [site, recordMap, lite])
+
+  const keys = Object.keys(recordMap?.block || {})
+  const block = recordMap?.block?.[keys[0]]?.value
+
+  // const isRootPage =
+  //   parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
+  const isBlogPost =
+    block?.type === 'page' && block?.parent_table === 'collection'
+
+  const showTableOfContents = !!isBlogPost
+  const minTableOfContentsItems = 3
+
+  const pageAside = React.useMemo(
+    () => (
+      <PageAside block={block} recordMap={recordMap} isBlogPost={isBlogPost} />
+    ),
+    [block, recordMap, isBlogPost]
+  )
+
+  //const footer = React.useMemo(() => <Footer />, [])
 
   if (router.isFallback) {
     return <Loading />
   }
 
-  if (!recordMap) {
-    return null
+  if (error || !site || !block) {
+    return <Page404 site={site} pageId={pageId} error={error} />
   }
 
-  const title = getPageTitle(recordMap)
-  console.log(title, recordMap)
+  const title = getBlockTitle(block, recordMap) || site.name
 
-  // useful for debugging from the dev console
-  if (typeof window !== 'undefined') {
-    const keys = Object.keys(recordMap?.block || {})
-    const block = recordMap?.block?.[keys[0]]?.value
+  if (!config.isServer) {
+    // add important objects to the window global for easy debugging
     const g = window as any
+    g.pageId = pageId
     g.recordMap = recordMap
     g.block = block
   }
 
-  const socialDescription = 'React Notion X Demo'
-  const socialImage =
-    'https://react-notion-x-demo.transitivebullsh.it/social.jpg'
+  const canonicalPageUrl =
+    !config.isDev && getCanonicalPageUrl(site, recordMap)(pageId)
+
+  const socialImage = mapImageUrl(
+    getPageProperty<string>('Social Image', block, recordMap) ||
+      (block as PageBlock).format?.page_cover ||
+      config.defaultPageCover,
+    block
+  )
+
+  const socialDescription =
+    getPageProperty<string>('Description', block, recordMap) ||
+    config.description
 
   return (
     <>
-      <Head>
-        {socialDescription && (
-          <>
-            <meta name='description' content={socialDescription} />
-            <meta property='og:description' content={socialDescription} />
-            <meta name='twitter:description' content={socialDescription} />
-          </>
-        )}
+      <PageHead
+        pageId={pageId}
+        site={site}
+        title={title}
+        description={socialDescription}
+        image={socialImage}
+        url={canonicalPageUrl}
+      />
 
-        {socialImage ? (
-          <>
-            <meta name='twitter:card' content='summary_large_image' />
-            <meta name='twitter:image' content={socialImage} />
-            <meta property='og:image' content={socialImage} />
-          </>
-        ) : (
-          <meta name='twitter:card' content='summary' />
-        )}
-
-        <title>{title}</title>
-        <meta property='og:title' content={title} />
-        <meta name='twitter:title' content={title} />
-        <meta name='twitter:creator' content='@transitive_bs' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      {isLiteMode && <BodyClassName className='notion-lite' />}
+      {isDarkMode && <BodyClassName className='dark-mode' />}
 
       <NotionRenderer
+        bodyClassName={cs(
+          styles.notion,
+          pageId === site.rootNotionPageId && 'index-page'
+        )}
+        darkMode={isDarkMode}
+        components={components}
         recordMap={recordMap}
-        fullPage={true}
-        darkMode={false}
-        rootDomain={rootDomain}
-        rootPageId={rootPageId}
-        previewImages={previewImagesEnabled}
-        components={{
-          // NOTE (transitive-bullshit 3/12/2023): I'm disabling next/image for this repo for now because the amount of traffic started costing me hundreds of dollars a month in Vercel image optimization costs. I'll probably re-enable it in the future if I can find a better solution.
-          // nextImage: Image,
-          nextLink: Link,
-          Code,
-          Collection,
-          Equation,
-          Pdf,
-          Modal,
-          Tweet
-        }}
-
-        // NOTE: custom images will only take effect if previewImages is true and
-        // if the image has a valid preview image defined in recordMap.preview_images[src]
+        rootPageId={site.rootNotionPageId}
+        rootDomain={site.domain}
+        fullPage={!isLiteMode}
+        previewImages={!!recordMap.preview_images}
+        showCollectionViewDropdown={false}
+        showTableOfContents={showTableOfContents}
+        minTableOfContentsItems={minTableOfContentsItems}
+        defaultPageIcon={config.defaultPageIcon}
+        defaultPageCover={config.defaultPageCover}
+        defaultPageCoverPosition={config.defaultPageCoverPosition}
+        mapPageUrl={siteMapPageUrl}
+        mapImageUrl={mapImageUrl}
+        searchNotion={config.isSearchEnabled ? searchNotion : null}
+        pageAside={pageAside}
+        pageFooter={
+          config.enableComment ? (
+            !isBlogPost ? null : (
+              <Comments pageId={pageId} recordMap={recordMap} />
+            )
+          ) : null
+        }
+        footer={null}
       />
     </>
   )
