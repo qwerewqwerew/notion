6c13ef7ea55b8c1959bb51f61a7e1153ef9ab32b
qwerewqwerew <qwerew0@gmail.com>
2024-09-09 15:53:54 +0900

    withBundleAnalyzer

--- next.config.js
+++ next.config.js
@@ -3,29 +3,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 600,
+  staticPageGenerationTimeout: 300,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.notion.so'
-      },
-      {
-        protocol: 'https',
-        hostname: 'notion.so'
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
-      },
-      {
-        protocol: 'https',
-        hostname: '**.twimg.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 's3.*.amazonaws.com'
-      }
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import type { AppProps } from 'next/app'
-//import { useRouter } from 'next/router'
-//import * as Fathom from 'fathom-client'
+import { useRouter } from 'next/router'
+
+import * as Fathom from 'fathom-client'
 import 'katex/dist/katex.min.css'
-//import posthog from 'posthog-js'
+import posthog from 'posthog-js'
 import 'prismjs/themes/prism-coy.css'
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
@@ -26,57 +27,57 @@ if (!isServer) {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  //   const router = useRouter()
-  //
-  //   React.useEffect(() => {
-  //     function updateNestedClasses() {
-  //       const bodyClass = document.body.className
-  //       const nestedElements = document.querySelectorAll(
-  //         '.dark-mode, .light-mode'
-  //       )
-  //
-  //       nestedElements.forEach((element) => {
-  //         if (bodyClass.includes('dark-mode')) {
-  //           element.classList.add('dark-mode')
-  //           element.classList.remove('light-mode')
-  //         } else {
-  //           element.classList.add('light-mode')
-  //           element.classList.remove('dark-mode')
-  //         }
-  //       })
-  //     }
-  //
-  //     function onRouteChangeComplete() {
-  //       if (fathomId) {
-  //         Fathom.trackPageview()
-  //       }
-  //
-  //       if (posthogId) {
-  //         posthog.capture('$pageview')
-  //       }
-  //
-  //       // Update nested classes on route change
-  //       updateNestedClasses()
-  //     }
-  //
-  //     if (fathomId) {
-  //       Fathom.load(fathomId, fathomConfig)
-  //     }
-  //
-  //     if (posthogId) {
-  //       posthog.init(posthogId, posthogConfig)
-  //     }
-  //
-  //     router.events.on('routeChangeComplete', onRouteChangeComplete)
-  //
-  //     // Initial update
-  //     updateNestedClasses()
-  //
-  //     // Cleanup
-  //     return () => {
-  //       router.events.off('routeChangeComplete', onRouteChangeComplete)
-  //     }
-  //   }, [router.events])
+  const router = useRouter()
+
+  React.useEffect(() => {
+    //     function updateNestedClasses() {
+    //       const bodyClass = document.body.className
+    //       const nestedElements = document.querySelectorAll(
+    //         '.dark-mode, .light-mode'
+    //       )
+    //
+    //       nestedElements.forEach((element) => {
+    //         if (bodyClass.includes('dark-mode')) {
+    //           element.classList.add('dark-mode')
+    //           element.classList.remove('light-mode')
+    //         } else {
+    //           element.classList.add('light-mode')
+    //           element.classList.remove('dark-mode')
+    //         }
+    //       })
+    //     }
+    //
+    //     function onRouteChangeComplete() {
+    //       if (fathomId) {
+    //         Fathom.trackPageview()
+    //       }
+    //
+    //       if (posthogId) {
+    //         posthog.capture('$pageview')
+    //       }
+    //
+    //       // Update nested classes on route change
+    //       updateNestedClasses()
+    //     }
+    //
+    //     if (fathomId) {
+    //       Fathom.load(fathomId, fathomConfig)
+    //     }
+    //
+    //     if (posthogId) {
+    //       posthog.init(posthogId, posthogConfig)
+    //     }
+    //
+    //     router.events.on('routeChangeComplete', onRouteChangeComplete)
+    //
+    //     // Initial update
+    //     updateNestedClasses()
+    //
+    //     // Cleanup
+    //     return () => {
+    //       router.events.off('routeChangeComplete', onRouteChangeComplete)
+    //     }
+  }, [router.events])
 
   return <Component {...pageProps} />
 }
