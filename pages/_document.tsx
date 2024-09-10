d9a684e652dfddf417d8e0ee9e39bb30a23b38f7
qwerewqwerew <qwerew0@gmail.com>
2024-09-10 14:32:40 +0900

    dark

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,30 @@
-import * as React from 'react'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { IconContext } from "@react-icons/all-files";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import * as React from "react";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='ko'>
-        <Head>
-          <link rel='shortcut icon' href='/favicon.ico' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
-          <link rel='manifest' href='/manifest.json' />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+      <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
+        <Html lang="ko">
+          <Head>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon.png"
+            />
+            <link rel="manifest" href="/manifest.json" />
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      </IconContext.Provider>
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
