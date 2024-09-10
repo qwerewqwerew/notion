d3744879a84a59a40eadb4469c79abd6c50c7dc4
qwerewqwerew <qwerew0@gmail.com>
2024-09-10 18:56:43 +0900

    codemod

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -28,7 +28,7 @@ export default async function OGImage(req: NextRequest) {
     return new Response(pageInfoRes.statusText, { status: pageInfoRes.status })
   }
   const pageInfo: NotionPageInfo = await pageInfoRes.json()
-  console.log(pageInfo)
+  // console.log(pageInfo)
 
   return new ImageResponse(
     (
