cc878052ff57ec7521fed58436075625bfa988da
qwerewqwerew <qwerew0@gmail.com>
2024-09-11 13:33:41 +0900

    config id 수정

--- site.config.ts
+++ site.config.ts
@@ -1,9 +1,8 @@
 import { siteConfig } from './lib/site-config';
 
-
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '660d1a8fa22a457ebab29e3d8c8af854',
+  rootNotionPageId: 'CoalaCoding-660d1a8fa22a457ebab29e3d8c8af854',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
